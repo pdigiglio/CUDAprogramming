7fa8cff1e520be2b3d256dfbbded83148af31309
pdigiglio <p.digiglio91@gmail.com>
2015-05-23 12:09:07 +0200

    first implementation of Runge-Kutta (+ change F() signature)

--- N-body/integrator.cc
+++ N-body/integrator.cc
@@ -9,16 +9,56 @@
  */
 //__device__  __host__
 void leapfrogVerlet ( float *x, float *v, size_t N ) {
-	for ( int i = 0; i < N; ++ i ) {
+	for ( size_t i = 0; i < N; ++ i ) {
 		x[i] += v[i] * dt;
-		v[i] += F( x, N ) * dt; 
+		v[i] += F( x[i], N ) * dt; 
 	}
 }
 
-int rungeKutta () {
-	return 1;
+	void
+rungeKutta ( float *x, float *v, size_t N ) {
+
+	float tmpX = 0.f, incrementX = 0.f;
+	float tmpV = 0.f, incrementV = 0.f;
+
+	for ( size_t i = 0; i < N; ++ i ) {
+	
+		incrementX = 0.f;
+		incrementV = 0.f;
+
+		// k1
+		tmpX = v[i];
+		tmpV = F( x[i], N );
+
+		// save increment
+		incrementX += tmpX;
+		incrementV += tmpV;
+
+		// k2
+		tmpX = v[i] + .5 * dt * tmpX;
+		tmpV = F( x[i] + .5 * dt * tmpV );
+
+		// save increment
+		incrementX += 2 * tmpX;
+		incrementV += 2 * tmpV;
+
+		// k3
+		tmpX = v[i] + .5 * dt * tmpX;
+		tmpV = F( x[i] + .5 * dt * tmpV );
+
+		// save increment
+		incrementX += 2 * tmpX;
+		incrementV += 2 * tmpV;
+
+		// k4
+		tmpX = v[i] + dt * tmpX;
+		tmpV = F( x[i] + dt * tmpV );
+
+		x[i] += ( incrementX + tmpX ) * dt / 6;
+		v[i] += ( incrementV + tmpV ) * dt / 6;
+	}
 }
 
-float F ( float *x, size_t N ) {
+float F ( float x, size_t N ) {
 	return .5f;
 }
