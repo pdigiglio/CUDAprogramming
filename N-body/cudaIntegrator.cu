937128f99e625970efde9244a189f748f87a97e4
pdigiglio <p.digiglio91@gmail.com>
2015-05-24 23:27:47 +0200

    add CUDA headers with a dummy trial() function

---
+++ N-body/cudaIntegrator.cu
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#include "integrator.h"
+#include "cudaIntegrator.h"
+
+__global__ void trial () {
+	unsigned threadID = blockDim.x * blockIdx.x + threadIdx.x;
+
+	printf( "Hello from kernel %u\n", threadID );
+
+	__syncthreads();
+}
---
+++ N-body/cudaIntegrator.h
@@ -0,0 +1,7 @@
+#ifndef CUDAINTEGRATOR_H_
+#define CUDAINTEGRATOR_H_
+
+__global__ void trial ();
+
+#endif /* CUDAINTEGRATOR_H_ */
+
