628aaf9eab000181efac7232b9d98d799c574593
pdigiglio <p.digiglio91@gmail.com>
2015-06-18 14:52:44 +0200

    add helper function for memory copying

--- N-body/cudaIntegrator.h
+++ N-body/cudaIntegrator.h
@@ -13,10 +13,45 @@ void distance ( const T *x_i, const T *x_j, T *x_ij ) {
     x_ij[2] = x_i[2] - x_j[2];
 };
 
+/**
+ * @brief Fetch memory from global to shared.
+ *
+ * In principle that's just a memory copy, irrespective of whether the memory
+ * is copyed from or to the gobal memory.
+ */
+template <size_t D,typename T>
+__device__
+inline void fetchFromGlobalMemory ( T *mySharedArray, const T *x ) {
+	for( size_t d = 0; d < D; ++ d )
+		mySharedArray[d] = x[d];
+}
+
+/**
+ * @brief Helper function to copy memory.
+ *
+ * This is the same function as `fetchFromGlobalMemory()` but I call it differently
+ * to keep the m logically separated.
+ */
+template <size_t D,typename T>
+__device__
+inline void writeToGlobalMemory ( T *x, const T *mySharedArray ) {
+	fetchFromGlobalMemory<D>( x, mySharedArray );
+}
+
 template <size_t N, size_t D, typename T>
 __global__ 
 void cudaLeapFrogVerlet( T* x, T* v ) {
-    unsigned threadID = blockDim.x * blockIdx.x + threadIdx.x;
+	__shared__ T    evolvingParticle[ D * 32 ];
+//	__shared__ T surroungingParticle[ D * 32 ];
+
+    unsigned i = blockDim.x * blockIdx.x + threadIdx.x;
+	fetchFromGlobalMemory <D> ( evolvingParticle + i, x + i ); 
+
+	evolvingParticle[i] ++;
+
+	writeToGlobalMemory<D>( x + i, evolvingParticle + i );
+
+
 
 //    printf( "Args [%u]: x %p y %p N %zu D %zu\n", threadID, (void*) x, (void*) v, N , D );
     // if you do like that then it may be that some other thread
@@ -25,8 +60,6 @@ void cudaLeapFrogVerlet( T* x, T* v ) {
 //    printf( "block %u (of %u), %u\n", blockIdx.y, gridDim.y, threadIdx.y );
 //    printf( "block %u (of %u), %u\n", blockIdx.z, gridDim.z, threadIdx.z );
 
-    x[ threadID ] ++;
-    
 };
 
 
